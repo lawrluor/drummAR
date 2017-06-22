9ab119da31dd52a687f70b133ea77137eed47823
Lawrence Luo <luolawrence1@gmail.com>
2017-06-22 10:25:02 -0400

    working collisions

---
+++ Assets/Scripts/SnareCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+public class SnareCollision : MonoBehaviour {
+    AudioSource SnareAudio;
+
+    void Start()
+    {
+        SnareAudio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        // activates on any part of the collision space
+        /*foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }*/
+        // checking impact magnitude
+        // if (collision.relativeVelocity.magnitude > 2)
+        SnareAudio.Play();
+    }
+}
